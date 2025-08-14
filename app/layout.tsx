30bde0e16f7ab3ce2e80b4b4afdc085dbb637105
Octavio Delpupo <octavio.delpupo@gmail.com>
2025-08-14 20:50:17 -0300

    chore: Iniciando projeto.

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Todo List App",
+  description: "Application to manage your todo list",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="mx-auto p-4 sm:max-w-xl md:max-w-2xl lg:max-w-6xl">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
